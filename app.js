86bc61187e8171b954dd637f85eb58605d39ba7d
ccahui <kris_ti4n@hotmail.com>
2018-12-21 20:05:36 -0500

    Se instalo Moongose para realizar esquemas de BD faciles de manejar, como un Objetos persistentes JDO en Java

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=>
+{
+    if(err) 
+        throw err;
+    console.log("Base de Datos: \x1b[32m%s\x1b[0m'",'online')
+});
 // Rutas
 app.get('/', (req, res, next) => {
     res.status(200).json({
